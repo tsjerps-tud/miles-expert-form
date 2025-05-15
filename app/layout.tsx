b0058c5085afc3c5026273be3e590cb095c06c64
sjerpsthomas <t.sjerps@student.tudelft.nl>
2025-05-15 17:19:50 +0200

    Change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
         <head>
             <link rel="icon" href="/favicon.svg" sizes="any" />
-            <title>Evaluation Form Experts</title>
+            <title>Peer Evaluation Form</title>
         </head>
         <body>
         <div className="flex flex-col w-full max-w-5xl mx-auto my-15 grow">
